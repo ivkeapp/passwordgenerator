724841a53b628719d3b2f5175099e753fab26715
Ivan Zarkovic <ivanzarkovic@yahoo.com>
2021-06-10 11:38:43 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="sr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Password Generator</title>
+		<meta name="description" content="The HTML5 Herald">
+		<meta name="author" content="Ivan Zarkovic">
+		<link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/dashboard/">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+		<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    </head>
+	<body>
+    
+    </body>
+</html>

5d9ab9948d6bd85cff6ee4dc56ea714e670e068f
Ivan Zarkovic <ivanzarkovic@yahoo.com>
2021-06-10 11:56:45 +0200

    Open file

--- generatepass.php
+++ generatepass.php
@@ -1,9 +1,17 @@
 <?php
 
-$wordlen = $_POST['wordlen'];
-$wordnum = $_POST['wordnum'];
-$changetonum = $_POST['changetonum'];
-$changetonum2 = $_POST['changetonum2'];
-$changetonum3 = $_POST['changetonum3'];
+    //TO-DO add isset or empty check
+    $wordlen = $_POST['wordlen'];
+    $wordnum = $_POST['wordnum'];
+    $changetonum = $_POST['changetonum'];
+    $changetonum2 = $_POST['changetonum2'];
+    $changetonum3 = $_POST['changetonum3'];
+
+    $words = array();
+    $fh = fopen('words.txt','r');
+    while ($line = fgets($fh)) {
+    array_push($words, trim($line));
+    }
+    fclose($fh);
 
 ?>
